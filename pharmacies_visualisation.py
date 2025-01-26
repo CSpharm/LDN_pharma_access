1af99b4b881dc04d5560841794ab11b993acb185
Wendy Lin <49999157+CSpharm@users.noreply.github.com>
2025-01-26 22:24:52 +0800

    Simple data check updated

--- pharmacies_visualisation.py
+++ pharmacies_visualisation.py
@@ -5,6 +5,37 @@ import pandas as pd
 file_path = '../data/london_pharmacies_geocoded.csv'
 df = pd.read_csv(file_path)
 
+# Get basic information
+print(df.info())
+
+print(f"Number of rows: {df.shape[0]}\n")
+print(f"Number of columns: {df.shape[1]}\n")
+
+# # Display the first 5 rows
+# print(df.head())
+
+# # Display the last 5 rows
+# print(df.tail())
+
+# # Display a random sample of 5 rows
+# print(df.sample(5))
+
+# Check for missing values
+print(df.isnull().sum())
+
+# Display rows with missing data
+missing_data = df[df.isnull().any(axis=1)]
+print(missing_data)
+
+# Check for duplicate rows
+duplicates = df[df.duplicated()]
+print(f"Number of duplicate rows: {len(duplicates)}")
+
+# Explore unique pharmacy names
+unique_names = df['Name'].unique()
+print(f"Number of unique pharmacy names: {len(unique_names)}")
+print(df.describe(include='all'))
+
 # Initialize a map centered around London
 london_map = folium.Map(location=[51.5074, -0.1278], zoom_start=11)
 
